a8a019b27c3161b8e1ae323a6e54dab22796cc9a
biao1997 <2013208937@qq.com>
2019-08-07 16:30:50 +0800

    second

---
+++ src/views/home/goods/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="goods"> goods组件</div>
+</template>
+<script>
+export default {
+  data () { return {} },
+  created () {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+</style>
